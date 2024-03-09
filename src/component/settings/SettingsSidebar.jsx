df090af86067ceaeace453833277c9807b758cbf
Manish Gupta <manish.info2020@gmail.com>
2024-03-09 17:25:42 +0530

    added variant and screenshots

--- src/component/settings/SettingsSidebar.jsx
+++ src/component/settings/SettingsSidebar.jsx
@@ -5,6 +5,9 @@ function SettingsSidebar() {
   return (
     <aside className="col-span-3 border-r border-bgray-200 dark:border-darkblack-400">
       <div className="px-4 py-6">
+      <div className="px-8">
+        <Progressbar className="bg-bgray-200 dark:bg-darkblack-500 p-7 rounded-xl" />
+      </div>
         <TabBtn
           link="#"
           title="Personal Informations"
@@ -167,10 +170,7 @@ function SettingsSidebar() {
           </svg>
         </TabBtn>
       </div>
-      {/* Progressbar  */}
-      <div className="px-8">
-        <Progressbar className="bg-bgray-200 dark:bg-darkblack-500 p-7 rounded-xl" />
-      </div>
+
     </aside>
   );
 }
