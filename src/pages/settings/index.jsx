52c829285cda21b772fddd80839b57f525f0e79b
Manish Gupta <manish.info2020@gmail.com>
2024-03-12 17:54:06 +0530

    added variant and screenshots

--- src/pages/settings/index.jsx
+++ src/pages/settings/index.jsx
@@ -6,11 +6,11 @@ function Settings() {
   return (
     <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px]">
       <div className="grid grid-cols-1 xl:grid-cols-12 bg-white dark:bg-darkblack-600 rounded-xl">
-        <SettingsSidebar />
         <div className="py-8 px-10 col-span-9 tab-content">
           <PersonalInfo />
         </div>
       </div>
+      <SettingsSidebar />
     </main>
   );
 }
